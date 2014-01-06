fe1ef4746162af7eaaa1a772f902cf98980d3ce2
Vadim Makeev <pepelsbey@gmail.com>
2014-01-06 21:49:32 +0400

    csso removed due to MQ optimization bug

--- Gruntfile.js
+++ Gruntfile.js
@@ -16,13 +16,6 @@ module.exports = function(grunt) {
 				src: 'styles/screen.css'
 			}
 		},
-		csso: {
-			minify: {
-				files: {
-					'styles/screen.css' : 'styles/screen.css'
-				}
-			}
-		},
 		jekyll: {
 			build: {}
 		},
@@ -76,7 +69,6 @@ module.exports = function(grunt) {
 
 	grunt.loadNpmTasks('grunt-contrib-sass');
 	grunt.loadNpmTasks('grunt-autoprefixer');
-	grunt.loadNpmTasks('grunt-csso');
 	grunt.loadNpmTasks('grunt-jekyll');
 	grunt.loadNpmTasks('grunt-contrib-watch');
 	grunt.loadNpmTasks('grunt-contrib-htmlmin');
@@ -86,7 +78,6 @@ module.exports = function(grunt) {
 	grunt.registerTask('default', [
 		'sass',
 		'autoprefixer',
-		'csso',
 		'jekyll',
 		'htmlmin',
 		'imagemin'
