6df87e862eee71ddf0de41a1b303e819a7a904ed
Vadim Makeev <pepelsbey@gmail.com>
2015-09-30 23:25:57 +0200

    Clean template

--- gulpfile.js
+++ gulpfile.js
@@ -15,21 +15,46 @@ var autoprefixer = require('gulp-autoprefixer'),
 	shell = require('gulp-shell'),
 	sync = require('browser-sync').create();
 
+// -------------------------------
 // Default
+// -------------------------------
 
-gulp.task('default', function(callback) {
-	sequence(
-		'jekyll', ['html', 'styles'], 'init', callback
-	);
+gulp.task('default', function() {
+
 });
 
-gulp.task('init', function() {
+// -------------------------------
+// Build
+// -------------------------------
+
+gulp.task('build', function() {
+
+});
+
+// -------------------------------
+// Deploy
+// -------------------------------
+
+gulp.task('deploy', function() {
+
+});
+
+// Server
+
+gulp.task('server', function() {
+
 	sync.init({
 		notify: false,
 		server: {
 			baseDir: 'dest'
 		}
 	});
+
+});
+
+// Watch
+
+gulp.task('watch', function() {
 
 	gulp.watch([
 		'src/styles/**/*.scss'
@@ -43,85 +68,154 @@ gulp.task('init', function() {
 			'jekyll', 'html', callback
 		);
 	});
-});
 
-gulp.task('html', function() {
-	return gulp.src([
-		'dest/**/index.html',
-		'dest/errors/*.html'
-		])
-		.pipe(beml({
-			elemPrefix: '__',
-			modPrefix: '--' }))
-		.pipe(htmlmin({
-			removeComments: true,
-			collapseWhitespace: true
-		}))
-		.pipe(gulp.dest('dest/'))
-		.pipe(sync.stream());
 });
 
 // Jekyll
 
-var jekyll = 'jekyll build --source src --destination dest';
+gulp.task('jekyll', function() {
 
-gulp.task('jekyll', shell.task([
-	jekyll + ' --limit_posts 50'
-]));
+});
+
+gulp.task('jekyll:limit', function() {
+
+});
+
+// HTML
+
+gulp.task('html', function() {
+
+});
 
 // Styles
 
-gulp.task('styles', function () {
-	return gulp.src('src/styles/screen.scss')
-		.pipe(sass())
-		.pipe(autoprefixer())
-		.pipe(cssmin())
-		.pipe(gulp.dest('dest/styles/'))
-		.pipe(sync.stream());
+gulp.task('styles', function() {
+
 });
 
 // Cache
 
-function hashEight(files) {
-	return hash.sync({
-		files: files
-	}).substring(0, 8);
-}
+gulp.task('cache', function() {
 
-gulp.task('cache:sw', function() {
-	gulp.src('dest/service-worker.js')
-		.pipe(replace(
-			/(const HASH = ')(';)/g,
-			'$1' + hashEight([
-				'dest/styles/screen.css',
-				'dest/images/github.svg',
-				'dest/images/logo.svg',
-				'dest/scripts/highlight.js',
-				'dest/scripts/salvattore.js']) + '$2'
-		))
-		.pipe(replace(
-			/('\/styles\/)(screen)(\.css',)/g,
-			'$1' + hashEight(['dest/styles/screen.css']) + '$3'
-		))
-		.pipe(gulp.dest('dest/'));
 });
 
-gulp.task('cache:links', function() {
-	gulp.src([
-		'**/index.html',
-		'errors/*.html'
-		], { cwd: 'dest' })
-		.pipe(replace(
-			/(<link rel="stylesheet" href="\/styles\/)(screen)(\.css">)/g,
-			'$1' + hashEight(['dest/styles/screen.css']) + '$3'
-		))
-		.pipe(gulp.dest('dest/'));
-});
+// // Build
 
-gulp.task('cache:styles', function() {
-	gulp.src('dest/styles/screen.css')
-		.pipe(rename(function(path) {
-			path.basename = hashEight(['dest/styles/screen.css'])
-		}))
-		.pipe(gulp.dest('dest/styles/'));
-});
+// gulp.task('build', function(callback) {
+// 	sequence(
+// 		'jekyll', ['html', 'styles'], callback
+// 	);
+// });
+
+// // Default
+
+// gulp.task('default', function(callback) {
+// 	sequence(
+// 		'jekyll', ['html', 'styles'], 'init', callback
+// 	);
+// });
+
+// gulp.task('init', function() {
+// 	sync.init({
+// 		notify: false,
+// 		server: {
+// 			baseDir: 'dest'
+// 		}
+// 	});
+
+// 	gulp.watch([
+// 		'src/styles/**/*.scss'
+// 	], ['styles']);
+
+// 	gulp.watch([
+// 		'src/**/*.md',
+// 		'src/**/*.html'
+// 	], function(callback) {
+// 		sequence(
+// 			'jekyll', 'html', callback
+// 		);
+// 	});
+// });
+
+// gulp.task('html', function() {
+// 	return gulp.src([
+// 		'dest/**/index.html',
+// 		'dest/errors/*.html'
+// 		])
+// 		.pipe(beml({
+// 			elemPrefix: '__',
+// 			modPrefix: '--' }))
+// 		.pipe(htmlmin({
+// 			removeComments: true,
+// 			collapseWhitespace: true
+// 		}))
+// 		.pipe(gulp.dest('dest/'))
+// 		.pipe(sync.stream());
+// });
+
+// // Jekyll
+
+// var jekyll = 'jekyll build --source src --destination dest';
+
+// gulp.task('jekyll', shell.task([
+// 	jekyll
+// ]));
+
+// gulp.task('jekyll:limit', shell.task([
+// 	jekyll + ' --limit_posts 50'
+// ]));
+
+// // Styles
+
+// gulp.task('styles', function () {
+// 	return gulp.src('src/styles/screen.scss')
+// 		.pipe(sass())
+// 		.pipe(autoprefixer())
+// 		.pipe(cssmin())
+// 		.pipe(gulp.dest('dest/styles/'))
+// 		.pipe(sync.stream());
+// });
+
+// // Cache
+
+// function hashEight(files) {
+// 	return hash.sync({
+// 		files: files
+// 	}).substring(0, 8);
+// }
+
+// gulp.task('cache', function() {
+
+// 	gulp.src('dest/service-worker.js')
+// 		.pipe(replace(
+// 			/(const HASH = ')(';)/g,
+// 			'$1' + hashEight([
+// 				'dest/styles/screen.css',
+// 				'dest/images/github.svg',
+// 				'dest/images/logo.svg',
+// 				'dest/scripts/highlight.js',
+// 				'dest/scripts/salvattore.js']) + '$2'
+// 		))
+// 		.pipe(replace(
+// 			/('\/styles\/)(screen)(\.css',)/g,
+// 			'$1' + hashEight(['dest/styles/screen.css']) + '$3'
+// 		))
+// 		.pipe(gulp.dest('dest/'));
+
+// 	gulp.src([
+// 		'**/index.html',
+// 		'errors/*.html'
+// 		], { cwd: 'dest' })
+// 		.pipe(replace(
+// 			/(<link rel="stylesheet" href="\/styles\/)(screen)(\.css">)/g,
+// 			'$1' + hashEight(['dest/styles/screen.css']) + '$3'
+// 		))
+// 		.pipe(gulp.dest('dest/'));
+
+// 	gulp.src('dest/styles/screen.css')
+// 		.pipe(rename(function(path) {
+// 			path.basename = hashEight(['dest/styles/screen.css'])
+// 		}))
+// 		.pipe(gulp.dest('dest/styles/'));
+
+// });
