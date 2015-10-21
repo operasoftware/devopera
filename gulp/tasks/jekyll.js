fcfdba4ab867ef892990db3f6f547d3888496546
Vadim Makeev <pepelsbey@gmail.com>
2015-10-21 15:12:13 +0200

    Environment vars for cleaner local development

--- gulp/tasks/jekyll.js
+++ gulp/tasks/jekyll.js
@@ -3,12 +3,12 @@ var gulp = require('gulp'),
 
 // Jekyll
 
-var jekyll = 'jekyll build --source src --destination dest';
+var jekyll = 'jekyll build --config jekyll.yml --source src --destination dest';
 
 gulp.task('jekyll:full', shell.task([
-	jekyll
+	jekyll + ' JEKYLL_ENV=production'
 ]));
 
 gulp.task('jekyll:limit', shell.task([
-	jekyll + ' --limit_posts 150'
+	jekyll + ' --limit_posts 150' + ' JEKYLL_ENV=development'
 ]));
